aee79f7ab1018a34a48beb3c3775b4072b38c99f
IdleGoat <rafie.amandio@ui.ac.id>
2022-09-13 17:31:55 +0700

    CS1_1

---
+++ rafieAmandioJSleepJS/JSleep.java
@@ -0,0 +1,65 @@
+package rafieAmandioJSleepJS;
+
+
+/**
+ * Write a description of class JSleep here.
+ *
+ * @author Rafie Amandio Fauzan
+ * @version 13-09-2022
+ */
+public class JSleep
+{
+    public static void main(String[] args) {
+
+    }
+
+    public static int getHotelId(){
+        return 0;
+    }
+    public static String getHotelName(){
+        return "hotel";
+    }
+
+    public static boolean isDiscount(){
+        return true;
+    }
+
+    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount){
+        if(beforeDiscount < afterDiscount){
+            return 0;
+        } else if (beforeDiscount == 0) {
+            return 0;
+        } else {
+            float selisih = beforeDiscount - afterDiscount;
+            return (selisih/beforeDiscount)*100;
+        }
+    }
+
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage > 100.0f){
+            return 0;
+        } else {
+            return (int)(price*((100.0f-discountPercentage)/100.0f));
+        }
+    }
+
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        float discount = (100.0f-discountPercentage)/100.0f;
+        return (int)(discountedPrice/discount);
+    }
+
+    public static float getAdminFeePercentage(){
+        return 0.05f;
+    }
+
+    public static int getAdminFee(int price){
+        return (int)(price*getAdminFeePercentage());
+    }
+
+    public static int getTotalPrice(int price,int numberOfNight){
+        int perNight = price* numberOfNight;
+        return perNight + getAdminFee(perNight);
+    }
+
+
+}
